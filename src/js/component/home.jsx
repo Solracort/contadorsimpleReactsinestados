64efea1014949e65ed18534f3e52720e4df0aa3d
Solracort <solracort@gmail.com>
2023-01-15 18:09:52 +0000

    contador naranja

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+//import { WatchIgnorePlugin } from "webpack";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+
+		<div className="padre"> 
+            <div className="contador uno">
+                <i className="fa fa-clock"></i>
+            </div>
+            <div className="contador siete"> {props.decenahora}</div>
+            <div className="contador seis">{props.unidadhora}:</div>
+            <div className="contador cinco">{props.decenaminuto}</div>
+            <div className="contador cuatro">{props.unidadminuto}:</div>
+            <div className="contador tres">{props.decenasegundo%10}</div>
+			<div className="contador dos">{props.unidadsegundo%10}</div>
+    	</div> 
+	)
+			/*{/* <h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
@@ -18,9 +31,8 @@ const Home = () => {
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
-			</p>
-		</div>
-	);
+			</p> */
+		
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,37 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+let seg = 0;
+let decseg =0;
+let min = 0;
+let decmin = 0;
+let hora = 0;
+let dechora = 0;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(function (){
+    
+    seg++ ;
+    if (seg  >9 ){
+        seg=0;
+        decseg++;
+        if (decseg>5){
+            min++;
+            decseg=0;
+            if(min>9){
+                min=0;
+                decmin++;
+                if(decmin>5){
+                    hora++;
+                    decmin=0;
+                    if(hora>9){
+                        dechora++;
+                        hora=0;
+                    }
+                }
+            }
+        }
+    };   
+
+ReactDOM.render(<Home    decenahora={dechora} unidadhora = {hora} decenaminuto = {decmin} unidadminuto = {min} decenasegundo = {decseg} unidadsegundo = {seg} />, document.querySelector("#app"));
+
+}, 1000)
